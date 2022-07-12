13a32c250368362e1fa0a85a046b34d3d561e753
Jumanazar <jimmy@sweetk.co.kr>
2022-07-12 09:28:21 +0900

    express and view engine setup

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
-
 const app = express();
 
-console.log("hello");
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname+"/public"));
+app.get("/", (req, res) => res.render("home"));
 
-app.listen(3000);
+const handleListen = () => console.log("Listening on http://localhost:3001");
+app.listen(3001, handleListen);
