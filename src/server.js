4be1c2dd26a3f9c7b2ee232f725059d6df54cdc0
Ubuntu <ubuntu@ip-172-26-13-156.eu-central-1.compute.internal>
2025-11-08 04:52:32 +0000

    added remote changes

--- src/server.js
+++ src/server.js
@@ -10,15 +10,31 @@ const PORT = 3000;
 // View + static setup
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
+<<<<<<< Updated upstream
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
+=======
+app.use("/zoom", express.static(__dirname+"/public"));
+app.get("/zoom/health", (_, res) => res.status(200).send("OK"));
+app.get(["/zoom","/zoom/"], (_, res) => res.render("home"));
+app.get("/", (_, res) => res.redirect("/zoom"));
+>>>>>>> Stashed changes
 
 
 // HTTP + Socket.io
 const httpServer = http.createServer(app);
+<<<<<<< Updated upstream
 const wsServer = new Server(httpServer, {
     cors: { origin: ["https://admin.socket.io"], credentials: true },
+=======
+const wsServer = Server(httpServer, {
+    cors: {
+        origin: ["https://admin.socket.io"],
+        credentials: true,
+    },
+    path: "/zoom/socket.io/"
+>>>>>>> Stashed changes
 });
 instrument(wsServer, { auth: false });
 
